ded3fdb604adbc5c9a4754cb63b068d1630190de
Florian von Bock <f@vonbock.info>
2017-08-04 16:02:25 +0900

    do not write the tables out by default (using in GAE) where we have no fs

--- dot_tools/dot_parser.py
+++ dot_tools/dot_parser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Author: Tim Henderson
-#Email: tim.tadh@hackthology.com
-#For licensing see the LICENSE file in the top level directory.
+# Author: Tim Henderson
+# Email: tim.tadh@hackthology.com
+# For licensing see the LICENSE file in the top level directory.
 
 from ply import yacc
 from dot_lexer import tokens, Lexer
@@ -31,9 +31,11 @@ subgraph    :   [ subgraph [ ID ] ] '{' stmt_list '}'
 compass_pt  :   (n | ne | e | se | s | sw | w | nw | c | _)
 '''
 
-## If you are confused about the syntax in this file I recommend reading the
-## documentation on the PLY website to see how this compiler compiler's syntax
-## works.
+# If you are confused about the syntax in this file I recommend reading the
+# documentation on the PLY website to see how this compiler compiler's syntax
+# works.
+
+
 class Parser(object):
 
     tokens = tokens
@@ -41,12 +43,13 @@ class Parser(object):
     )
 
     def __new__(cls, **kwargs):
-        ## Does magic to allow PLY to do its thing.
+        # Does magic to allow PLY to do its thing.
         self = super(Parser, cls).__new__(cls, **kwargs)
         self.loc = list()
         self.yacc = yacc.yacc(
             module=self,
             tabmodule="dot_parser_tab",
+            write_tables=False,
             debug=0,
             **kwargs
         )
@@ -64,13 +67,15 @@ class Parser(object):
     def p_Graphs_1(self, t):
         'Graphs : Graph'
         t[0] = Node('Graphs').addkid(t[1])
+
     def p_Graphs_3(self, t):
         'Graphs : Graphs Graph'
-        t[0] = Node('Graphs', children=t[1].children+[t[2]])
+        t[0] = Node('Graphs', children=t[1].children + [t[2]])
 
     def p_Graph_1(self, t):
         'Graph : GraphStmt'
         t[0] = t[1]
+
     def p_Graph_2(self, t):
         'Graph : COMMENT'
         t[0] = Node('Comment').addkid(Node(t[1]))
@@ -78,12 +83,16 @@ class Parser(object):
     def p_GraphStmt_1(self, t):
         'GraphStmt : GraphType GraphBody'
         t[0] = Node('Graph').addkid(t[1]).addkid(Node(self.next_g_id())).addkid(t[2])
+
     def p_GraphStmt_2(self, t):
         'GraphStmt : GraphType ID GraphBody'
         t[0] = Node('Graph').addkid(t[1]).addkid(Node(t[2])).addkid(t[3])
+
     def p_GraphStmt_3(self, t):
         'GraphStmt : STRICT GraphType GraphBody'
-        t[0] = Node('Graph').addkid(t[2].addkid(Node('strict'))).addkid(Node(self.next_g_id())).addkid(t[3])
+        t[0] = Node('Graph').addkid(t[2].addkid(Node('strict'))
+                                    ).addkid(Node(self.next_g_id())).addkid(t[3])
+
     def p_GraphStmt_4(self, t):
         'GraphStmt : STRICT GraphType ID GraphBody'
         t[0] = Node('Graph').addkid(t[2].addkid(Node('strict'))).addkid(Node(t[3])).addkid(t[4])
@@ -98,6 +107,7 @@ class Parser(object):
     def p_GraphBody_1(self, t):
         'GraphBody : LCURLY StmtList RCURLY'
         t[0] = t[2]
+
     def p_GraphBody_3(self, t):
         'GraphBody : LCURLY RCURLY'
         t[0] = Node('Stmts')
@@ -108,12 +118,13 @@ class Parser(object):
                  | Stmt SEMI
         '''
         t[0] = Node('Stmts').addkid(t[1])
+
     def p_StmtList_2(self, t):
         '''
         StmtList : Stmt StmtList
                  | Stmt SEMI StmtList
         '''
-        stmts = t[len(t)-1]
+        stmts = t[len(t) - 1]
         kids = [t[1]] + stmts.children
         types = dict()
         for kid in kids:
@@ -121,25 +132,30 @@ class Parser(object):
             items += kid.children
             types[kid.label] = items
         children = list()
-        for k,v in types.iteritems():
+        for k, v in types.iteritems():
             children.append(Node(k, children=v))
         t[0] = Node('Stmts', children=children)
 
     def p_Stmt_1(self, t):
         'Stmt : ID EQUAL ID'
         t[0] = Node('Assigns').addkid(Node('=').addkid(t[1]).addkid(t[3]))
+
     def p_Stmt_2(self, t):
         'Stmt : NodeStmt'
         t[0] = Node('Nodes').addkid(t[1])
+
     def p_Stmt_3(self, t):
         'Stmt : EdgeStmt'
         t[0] = t[1]
+
     def p_Stmt_4(self, t):
         'Stmt : AttrStmt'
         t[0] = t[1]
+
     def p_Stmt_5(self, t):
         'Stmt : SubGraph'
         t[0] = t[1]
+
     def p_Stmt_6(self, t):
         'Stmt : COMMENT'
         t[0] = Node('Comments').addkid(t[1])
@@ -147,6 +163,7 @@ class Parser(object):
     def p_NodeStmt_1(self, t):
         'NodeStmt : NodeId'
         t[0] = Node('Node').addkid(t[1])
+
     def p_NodeStmt_2(self, t):
         'NodeStmt : NodeId AttrList'
         t[0] = Node('Node').addkid(t[1]).addkid(Node('Attrs', children=t[2]))
@@ -154,22 +171,24 @@ class Parser(object):
     def p_NodeId_1(self, t):
         'NodeId : ID'
         t[0] = Node(t[1])
+
     def p_NodeId_2(self, t):
         'NodeId : ID Port'
         t[0] = Node(t[1]).addkid(t[2])
 
-    def p_Port_1(self,t):
+    def p_Port_1(self, t):
         'Port : COLON ID'
         t[0] = Node('Port').addkid(Node(t[2]))
-    def p_Port_2(self,t):
-        'Port : COLON ID COLON ID' # where the last id is a compass_pt
+
+    def p_Port_2(self, t):
+        'Port : COLON ID COLON ID'  # where the last id is a compass_pt
         if t[4] not in ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw', 'c', '_']:
             raise Exception("ID %s is not a compass point" % t[4])
         t[0] = Node('Port').addkid(Node(t[2])).addkid(Node(t[4]))
 
     def p_AttrStmt_1(self, t):
         'AttrStmt : AttrType AttrList'
-        t[0] = Node(t[1]+'Attrs', children=t[2])
+        t[0] = Node(t[1] + 'Attrs', children=t[2])
 
     def p_AttrType_1(self, t):
         '''
@@ -182,12 +201,15 @@ class Parser(object):
     def p_AttrList_1(self, t):
         'AttrList : LSQUARE RSQUARE'
         t[0] = []
+
     def p_AttrList_2(self, t):
         'AttrList : LSQUARE AList RSQUARE'
         t[0] = t[2]
+
     def p_AttrList_3(self, t):
         'AttrList : LSQUARE RSQUARE AttrList'
         t[0] = t[3]
+
     def p_AttrList_4(self, t):
         'AttrList : LSQUARE AList RSQUARE AttrList'
         t[0] = t[2] + t[4]
@@ -195,6 +217,7 @@ class Parser(object):
     def p_AList_1(self, t):
         'AList : AExpr'
         t[0] = [t[1]]
+
     def p_AList_2(self, t):
         'AList : AExpr AList'
         t[0] = [t[1]] + t[2]
@@ -225,6 +248,7 @@ class Parser(object):
     def p_EdgeReciever_1(self, t):
         'EdgeReciever : NodeId'
         t[0] = t[1]
+
     def p_EdgeReciever_2(self, t):
         'EdgeReciever : SubGraph'
         t[0] = t[1]
@@ -232,6 +256,7 @@ class Parser(object):
     def p_EdgeRHS_1(self, t):
         'EdgeRHS : EdgeOp EdgeReciever'
         t[0] = [(t[1], t[2])]
+
     def p_EdgeRHS_2(self, t):
         'EdgeRHS : EdgeOp EdgeReciever EdgeRHS'
         t[0] = [(t[1], t[2])] + t[3]
@@ -246,10 +271,11 @@ class Parser(object):
     def p_SubGraph_1(self, t):
         'SubGraph : GraphBody'
         t[0] = Node('SubGraph').addkid(Node(self.next_g_id())).addkid(t[1])
+
     def p_SubGraph_2(self, t):
         'SubGraph : SUBGRAPH GraphBody'
         t[0] = Node('SubGraph').addkid(Node(self.next_g_id())).addkid(t[2])
+
     def p_SubGraph_3(self, t):
         'SubGraph : SUBGRAPH ID GraphBody'
         t[0] = Node('SubGraph').addkid(Node(t[2])).addkid(t[3])
-
